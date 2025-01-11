99fe0e993baceee421bfea6e789e98247ed4055f
Briuwu <millontebrian@gmail.com>
2025-01-11 15:30:58 +0800

    Add message editing functionality and improve UI layout

---
+++ scripts/components/edit-message.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const messageId = parseInt(urlParams.get("id"));
+
+  if (isNaN(messageId)) {
+    window.location.href = `index.html`;
+  }
+
+  displayMessage(messageId);
+
+  const editInput = document.querySelector("#edit-message");
+  const editSection = document.querySelector("#edit-section");
+
+  // Close the message
+  document.querySelector(".close-button").addEventListener("click", () => {
+    window.location.href = `index.html`;
+  });
+
+  // Open the decryption modal
+  document.querySelector("#decrypt-button").addEventListener("click", () => {
+    document.getElementById("decrypt-modal").style.display = "block";
+  });
+
+  console.log(messageId);
+
+  document.querySelector("#edit-button").addEventListener("click", () => {
+    updateMessage(editInput.value, messageId);
+    window.location.href = `index.html`;
+  });
+
+  // Decrypt the message
+  document
+    .querySelector("#decrypt-modal #decrypt-button")
+    .addEventListener("click", () => {
+      const otp = document.getElementById("otp").value.trim();
+      const message = getMessagesById(messageId);
+      if (otp !== message.otp) {
+        showMesage("Wrong OTP");
+        return;
+      }
+      document.querySelector("#decrypt-modal").style.display = "none";
+      editSection.style.display = "block";
+      const decryptedMessage = otpDecryptCipher(message.message, otp);
+      editInput.value = decryptedMessage;
+      // showMesage(`Decrypted Message: ${decryptedMessage}`);
+    });
+
+  // Close the modal in decryption process
+  document
+    .querySelector("#decrypt-modal #close-button")
+    .addEventListener("click", () => {
+      document.getElementById("decrypt-modal").style.display = "none";
+      document.getElementById("otp").value = "";
+    });
+});
--- scripts/data/messages.js
+++ scripts/data/messages.js
@@ -1,58 +1,106 @@
 // Function to get messages from localStorage or initialize with default messages
 const getMessages = () => {
-    let messages = [];
+  let messages = [];
 
-    // Try to load message data from localStorage
-    const storedMessages = localStorage.getItem('messages');
-    if (storedMessages) {
-        messages = JSON.parse(storedMessages);
-    } else {
-        // If no data in localStorage, use default data
-        const defaultMessages = ["Hello World!", "Love is not blind!", "Pogi Ako", "BoomPanet", "Wassup", "Pogi Mo Sir!"];
-        messages = defaultMessages.map((msg, index) => {
-            const otp = generateOTP(msg.length);
-            return { id: index + 1, username: `user${index + 1}`, message: otpEncryptCipher(msg, otp), otp: otp, date: new Date() };
-        });
-        localStorage.setItem('messages', JSON.stringify(messages));
-    }
+  // Try to load message data from localStorage
+  const storedMessages = localStorage.getItem("messages");
+  if (storedMessages) {
+    messages = JSON.parse(storedMessages);
+  } else {
+    // If no data in localStorage, use default data
+    const defaultMessages = [
+      "Hello World!",
+      "Love is not blind!",
+      "Pogi Ako",
+      "BoomPanet",
+      "Wassup",
+      "Pogi Mo Sir!",
+    ];
+    messages = defaultMessages.map((msg, index) => {
+      const otp = generateOTP(msg.length);
+      return {
+        id: index + 1,
+        username: `user${index + 1}`,
+        message: otpEncryptCipher(msg, otp),
+        otp: otp,
+        date: new Date(),
+      };
+    });
+    localStorage.setItem("messages", JSON.stringify(messages));
+  }
 
-    return messages;
-}
+  return messages;
+};
 
 // Function to get users from localStorage or initialize with default users
 const getMessagesById = (id) => {
-    const messages = getMessages();
-    const message = messages.find(message => message.id === id);
-    return message;
-}
+  const messages = getMessages();
+  const message = messages.find((message) => message.id === id);
+  return message;
+};
 
 // Function to insert a new message
 const insertMessage = (newMessage) => {
-    const messages = getMessages();
-    messages.push(newMessage);
-    localStorage.setItem('messages', JSON.stringify(messages));
-}
+  const messages = getMessages();
+  messages.push(newMessage);
+  localStorage.setItem("messages", JSON.stringify(messages));
+};
 
 // Function to update a message
-const updateMessage = (updatedMessage) => {
-    const messages = getMessages();
-    const messageIndex = messages.findIndex(message => message.id === updatedMessage.id);
-    messages[messageIndex] = updatedMessage;
-    localStorage.setItem('messages', JSON.stringify(messages));
-}
+const updateMessage = (updatedMessage, id) => {
+  let messages = getMessages();
+  const message = messages.find((message) => message.id === id);
+
+  const updatedMsg = {
+    ...message,
+    message: updatedMessage,
+  };
+
+  localStorage.setItem(
+    "messages",
+    JSON.stringify([
+      ...messages.filter((message) => message.id !== id),
+      updatedMsg,
+    ])
+  );
+
+  const newMsgs = JSON.parse(localStorage.getItem("messages"));
+
+  messages = newMsgs.map((msg) => {
+    const otp = generateOTP(msg.message.length);
+    if (msg.id === id) {
+      return {
+        ...msg,
+        message: otpEncryptCipher(updatedMessage, otp),
+        otp,
+      };
+    } else {
+      return msg;
+    }
+  });
+
+  localStorage.setItem("messages", JSON.stringify(messages));
+};
 
 // Function to delete a message
 const deleteMessage = (messageId) => {
-    const messages = getMessages();
-    const messageIndex = messages.findIndex(message => message.id === messageId);
-    messages.splice(messageIndex, 1);
-    localStorage.setItem('messages', JSON.stringify(messages));
-}
+  if (!confirm("Are you sure you want to delete this message?")) return;
+
+  const messages = getMessages();
+  const messageIndex = messages.findIndex(
+    (message) => message.id === messageId
+  );
+  messages.splice(messageIndex, 1);
+  localStorage.setItem("messages", JSON.stringify(messages));
+  displayMessages();
+};
 
 const displayMessages = () => {
-    const messages = getMessages();
-    const messageContainer = document.querySelector('.message-container');
-    messageContainer.innerHTML = messages.map(message => `
+  const messages = getMessages();
+  const messageContainer = document.querySelector(".message-container");
+  messageContainer.innerHTML = messages
+    .map(
+      (message) => `
         <tr>
             <td>${message.id}</td>
             <td>${message.username}</td>
@@ -60,19 +108,27 @@ const displayMessages = () => {
             <td>${message.otp}</td>
             <td>${DateFormat(message.date)}</td>
             <td>
-                <button class="open-button" id="open-button" data-id="${message.id}">View</button>
-                <button class="edit-button" data-id="${message.id}">Edit</button>
-                <button class="delete-button" data-id="${message.id}">Delete</button>
+                <button class="open-button" data-id="${
+                  message.id
+                }">View</button>
+                <button class="edit-button" data-id="${
+                  message.id
+                }">Edit</button>
+                <button class="delete-button" data-id="${
+                  message.id
+                }" onclick="deleteMessage(${message.id})">Delete</button>
             </td>
         </tr>
-    `).join('');
-}
+    `
+    )
+    .join("");
+};
 
 const displayMessage = (messageId) => {
-    const message = getMessagesById(messageId);
-    console.log(message);
-    const messageContainer = document.querySelector('.message-container');
-    messageContainer.innerHTML = `
+  const message = getMessagesById(messageId);
+  console.log(message);
+  const messageContainer = document.querySelector(".message-container");
+  messageContainer.innerHTML = `
        <div class="message">
            <h2>Ciphered Message: ${message.message}</h2>
            <p>Message By: ${message.username}</p>
@@ -80,15 +136,22 @@ const displayMessage = (messageId) => {
            <p>Date: ${DateFormat(message.date)}</p>
            <strong>Open the message to decrypt</strong>
            <div class="button-container">
-                <button class="open-button" id="decrypt-button" data-id="${message.id}">Unlock Message</button>
+                <button class="open-button" id="decrypt-button" data-id="${
+                  message.id
+                }">Unlock Message</button>
                 <button class="close-button">Close</button>
            </div>
        </div>
     `;
-}
+};
 
 const DateFormat = (date) => {
-    const newDate = new Date(date);
-    const dateFormat = newDate.toLocaleString('default', { month: 'short' }) + ' ' + newDate.getDate() + ' '  + newDate.getFullYear();
-    return dateFormat;
-}
+  const newDate = new Date(date);
+  const dateFormat =
+    newDate.toLocaleString("default", { month: "short" }) +
+    " " +
+    newDate.getDate() +
+    " " +
+    newDate.getFullYear();
+  return dateFormat;
+};
